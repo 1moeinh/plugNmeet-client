edbc951eddae6f71aa8e2eb513edbdb6ee2b9843
Mohammad Moein <mohammadmoein138077@gmail.com>
2026-02-25 10:43:25 +0330

    fix(ui): add dark mode background support for other user messages

--- src/components/chat/messages/message/messageTypes.tsx
+++ src/components/chat/messages/message/messageTypes.tsx
@@ -66,7 +66,7 @@ export const OtherUserMessage = memo(({ body }: { body: ChatMessage }) => {
           </p>
         </div>
         <p
-          className="message-content py-1.5 3xl:py-2.5 px-2.5 3xl:px-3.5 border border-Gray-200 rounded-lg 3xl:rounded-2xl overflow-hidden text-sm 3xl:text-base text-Gray-950 dark:text-white break-words rounded-tl-none bg-Gray-50"
+          className="message-content py-1.5 3xl:py-2.5 px-2.5 3xl:px-3.5 border border-Gray-200 rounded-lg 3xl:rounded-2xl overflow-hidden text-sm 3xl:text-base text-Gray-950 dark:text-white break-words rounded-tl-none bg-Gray-50 dark:bg-Gray-800"
           dangerouslySetInnerHTML={{ __html: body.message }}
         />
       </div>
